17506ed96875ea086e641a529dcd392116e6c7b3
ryota_miyagawa <i31640.ryota@gmail.com>
2025-09-12 22:23:55 +0900

    feat:weztermの設定作成

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,153 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- 外観・テーマ設定
+config.color_scheme = "Batman"
+config.font = wezterm.font("MesloLGS NF", { weight = "Regular" })
+config.font_size = 12
+config.line_height = 0.9
+config.window_background_opacity = 0.95
+config.window_decorations = "RESIZE" -- TITLE | RESIZE | NONE から選択
+
+-- 設定ファイルの自動リロード
+config.automatically_reload_config = true
+
+-- アクティブ/非アクティブペインの視覚的区別
+config.inactive_pane_hsb = {
+	saturation = 0.9, -- 彩度を下げる
+	brightness = 0.4, -- 明度を下げる（暗くする）
+}
+
+-- キーバインド設定
+config.keys = {
+	-- shift + enter で改行（リテラル改行文字を送信）
+	{
+		key = "Enter",
+		mods = "SHIFT",
+		action = wezterm.action.SendString("\n"),
+	},
+	-- cmd + d で横に画面分割（ホームディレクトリで開く）
+	{
+		key = "d",
+		mods = "CMD",
+		action = wezterm.action.SplitPane({
+			direction = "Right",
+			command = { cwd = wezterm.home_dir },
+		}),
+	},
+	-- cmd + shift + d で縦方向に画面分割（ホームディレクトリで開く）
+	{
+		key = "d",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+			command = { cwd = wezterm.home_dir },
+		}),
+	},
+	-- cmd + t でタブ作成（ホームディレクトリで開く）
+	{
+		key = "t",
+		mods = "CMD",
+		action = wezterm.action.SpawnCommandInNewTab({
+			cwd = wezterm.home_dir,
+		}),
+	},
+	-- cmd + w で現在のペインを閉じる
+	{
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	-- cmd + 矢印 で分割した画面の移動
+	{
+		key = "LeftArrow",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "RightArrow",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "UpArrow",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "DownArrow",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	-- cmd + 数字で特定のタブに移動
+	{
+		key = "1",
+		mods = "CMD",
+		action = wezterm.action.ActivateTab(0),
+	},
+	{
+		key = "2",
+		mods = "CMD",
+		action = wezterm.action.ActivateTab(1),
+	},
+	{
+		key = "3",
+		mods = "CMD",
+		action = wezterm.action.ActivateTab(2),
+	},
+	{
+		key = "4",
+		mods = "CMD",
+		action = wezterm.action.ActivateTab(3),
+	},
+	{
+		key = "5",
+		mods = "CMD",
+		action = wezterm.action.ActivateTab(4),
+	},
+	{
+		key = "6",
+		mods = "CMD",
+		action = wezterm.action.ActivateTab(5),
+	},
+	{
+		key = "7",
+		mods = "CMD",
+		action = wezterm.action.ActivateTab(6),
+	},
+	{
+		key = "8",
+		mods = "CMD",
+		action = wezterm.action.ActivateTab(7),
+	},
+	{
+		key = "9",
+		mods = "CMD",
+		action = wezterm.action.ActivateTab(8),
+	},
+	-- cmd + a で全選択
+	{
+		key = "a",
+		mods = "CMD",
+		action = wezterm.action.SelectTextAtMouseCursor("SemanticZone"),
+	},
+	-- cmd + c でコピー
+	{
+		key = "c",
+		mods = "CMD",
+		action = wezterm.action.CopyTo("Clipboard"),
+	},
+	-- cmd + v でペースト
+	{
+		key = "v",
+		mods = "CMD",
+		action = wezterm.action.PasteFrom("Clipboard"),
+	},
+}
+
+return config
